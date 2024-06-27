6eb494a9aec9d3b929f1aa533be5103a5e418fc1
assac453 <ritterfrosch@yandex.ru>
2024-06-27 17:10:22 +0300

    init

---
+++ src/main/java/com/assac453/automatedworkstation/AutomatedworkstationApplication.java
@@ -0,0 +1,13 @@
+package com.assac453.automatedworkstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedworkstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedworkstationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assac453/automatedworkstation/AutomatedworkstationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assac453.automatedworkstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedworkstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
